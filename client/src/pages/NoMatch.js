92df782c36cdf0defc6d13cbf48ecb288d85caff
Key <jh.yield@gmail.com>
2022-08-01 18:53:02 +1000

    no match page

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text } from "@chakra-ui/react";
+
+const NoMatch = () => {
+  return (
+    <div>
+      <Text minHeight="200" m="5" fontSize="4xl">
+        Oops this page doesn't exist.
+      </Text>
+      <Text minHeight="10" m="6" fontSize="xl">
+        Click Go Back below or select options above
+      </Text>
+    </div>
+  );
+};
+
+export default NoMatch;
