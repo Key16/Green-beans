5e3c46c9ee2211242885e59f764e21cdc86e4459
Key <jh.yield@gmail.com>
2022-08-01 18:49:05 +1000

    moved utils folder

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,68 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+// export const ADD_ORDER = gql`
+//   mutation addOrder($products: [ID]!) {
+//     addOrder(products: $products) {
+//       purchaseDate
+//       products {
+//         _id
+//         name
+//         description
+//         price
+//         quantity
+//         category {
+//           name
+//         }
+//       }
+//     }
+//   }
+// `;
+
+export const ADD_BEAN = gql`
+  mutation addBean($title: String!, $description: String!) {
+    addBean(title: $title, description: $description) {
+      _id
+      title
+      description
+      image
+      beanAuthor
+      donation
+      createdAt
+      category {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,75 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_BEANS = gql`
+  query getBeans($category: ID) {
+    beans(category: $category) {
+      _id
+      title
+      description
+      image
+      beanAuthor
+      donation
+      createdAt
+      category {
+        _id
+      }
+      donaters {
+        _id
+        donatedAmount
+      }
+    }
+  }
+`;
+
+// export const QUERY_CHECKOUT = gql`
+//   query getCheckout($products: [ID]!) {
+//     checkout(products: $products) {
+//       session
+//     }
+//   }
+// `;
+
+// export const QUERY_ALL_PRODUCTS = gql`
+//   {
+//     products {
+//       _id
+//       name
+//       description
+//       price
+//       quantity
+//       category {
+//         name
+//       }
+//     }
+//   }
+// `;
+
+// export const QUERY_CATEGORIES = gql`
+//   {
+//     categories {
+//       _id
+//       name
+//     }
+//   }
+// `;
+
+// export const QUERY_USER = gql`
+//   {
+//     user {
+//       firstName
+//       lastName
+//       orders {
+//         _id
+//         purchaseDate
+//         products {
+//           _id
+//           name
+//           description
+//           price
+//           quantity
+//           image
+//         }
+//       }
+//     }
+//   }
+// `;
