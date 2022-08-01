d40c0806f5847a2230a28bf9d6b60f49a8debd3a
Key <jh.yield@gmail.com>
2022-08-01 18:52:40 +1000

    beans main pages

---
+++ client/src/pages/Beans.js
@@ -0,0 +1,15 @@
+import React from "react";
+import BeanList from "../components/BeanList";
+// import CategoryMenu from "../components/CategoryMenu";
+
+const Beans = () => {
+  return (
+    <div>
+      {/* <CategoryMenu /> */}
+
+      <BeanList />
+    </div>
+  );
+};
+
+export default Beans;
