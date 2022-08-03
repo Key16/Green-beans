77c25c43484cf5ab9c63bcdd965311394e9926a7
Key <jh.yield@gmail.com>
2022-08-03 21:20:48 +1000

    Single beans page

---
+++ client/src/pages/SingleBean.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_SINGLE_BEAN } from "../utils/queries";
+import Auth from "../utils/auth";
+
+import {
+  Box,
+  Wrap,
+  Spacer,
+  Text,
+  Button,
+  ButtonGroup,
+  Divider,
+} from "@chakra-ui/react";
+
+const Beans = () => {
+  const { beanId } = useParams();
+
+  const { loading, data } = useQuery(QUERY_SINGLE_BEAN, {
+    // pass URL parameter
+    variables: { beanId: beanId },
+  });
+
+  const bean = data?.bean || {};
+  console.log("data :>> ", data);
+  console.log("bean :>> ", bean);
+
+  function Authentication() {
+    if (Auth.loggedIn() && bean.donation) {
+      return (
+        <ButtonGroup gap="3">
+          <Button variant="outline" mr="2" colorScheme="teal">
+            Fund this Bean
+          </Button>
+        </ButtonGroup>
+      );
+    }
+  }
+
+  return (
+    <div>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <div>
+          <Wrap py={30} mx="10" mt="10" mb="5" spacing="30px">
+            <Box width={["100%", "50%"]} bg="white" key={bean._id}>
+              {bean.image ? (
+                <img src={bean.image} alt={bean.title} />
+              ) : (
+                <img
+                  src="https://h7.alamy.com/comp/W9P1GN/seamless-pattern-tile-cartoon-with-peas-and-beans-illustration-W9P1GN.jpg"
+                  alt={bean.title}
+                />
+              )}
+            </Box>
+            <Spacer />
+            <Box
+              width={[
+                "100%", // 0-30em
+                "40%", // 30em-48em
+              ]}
+              bg="white"
+            >
+              <Text fontSize="3xl">{bean.title}</Text>
+              <Text as="i" fontSize="md">
+                Posted by {bean.beanAuthor} on {bean.createdAt}
+              </Text>
+              <Divider />
+              <Text py={10} fontSize="2xl">
+                {bean.description}
+              </Text>
+              <Box width={["100%"]} height="160px">
+                {bean.donation ? (
+                  <Text mb="3" fontSize="2xl">
+                    {" "}
+                    Bean amount $ {bean.donation}{" "}
+                  </Text>
+                ) : null}{" "}
+                {Authentication()}
+              </Box>
+            </Box>
+          </Wrap>
+
+          <Wrap py={30} m="10" spacing="30px">
+            <Spacer />
+          </Wrap>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Beans;
