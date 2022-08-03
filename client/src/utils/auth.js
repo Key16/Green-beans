29be8d482b8191f44762bc29f83b86eb19810f4e
Key <jh.yield@gmail.com>
2022-08-03 23:06:42 +1000

    removed refresh on login

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -8,6 +8,7 @@ class AuthService {
   loggedIn() {
     // Checks if there is a saved token and it's still valid
     const token = this.getToken();
+    // console.log("token :>> ", token);
     return !!token && !this.isTokenExpired(token);
   }
 
@@ -30,8 +31,7 @@ class AuthService {
   login(idToken) {
     // Saves user token to localStorage
     localStorage.setItem("id_token", idToken);
-
-    window.location.assign("/Beans");
+    // window.location.assign("/profile");
   }
 
   logout() {
